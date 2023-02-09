5de415819df7bf5387224129a054f9847f983406
Will_09 <wbahoque71@gmail.com>
2023-02-09 14:08:23 -0500

    Inicio de proyecto citas-react, se añade el gestor de paquetes yarn, se eliminan algunos archivos y se modifican otros, se aprende algo de JS en jsx

---
+++ citas-project/src/App.jsx
@@ -0,0 +1,15 @@
+
+function App() {
+  
+  // <div className="App">
+  //     <h1>{'Hello World'.toLowerCase()}</h1> {/* Se pone los corchetes para realizar metodos JS y estos no se muestren como textos, solo se imprimen expresiones*/}
+  //   </div> Representacion jsx
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App;
