79a08df2779dd36898dea49cccdb258ecbfd9ce0
William 09 <wbahoque71@gmail.com>
2023-02-21 14:35:30 -0500

    Se crea un nuevo componente para visualizar varios pacientes creados y se modifica la vista de editar pacientes

--- citas-project/src/components/ListadoPaciente.jsx
+++ citas-project/src/components/ListadoPaciente.jsx
@@ -1,9 +1,23 @@
+import { Paciente } from './Paciente';
+
+
+
 
 export const ListadoPaciente = () => {
   return (
     <>
-      <div className='m-1/2  lg:w-3/5'>
-        <h2>ListadoPaciente</h2>
+      <div className="md:m-1/2  lg:w-3/5 md:h-screen overflow-y-scroll">
+        <h2 className="font-black text-3xl text-center">Listado Pacientes</h2>
+        <p className="text-xl mt-5 mb-10 text-center">
+          Administra tus {""}
+          <span className="text-indigo-600 font-bold">Pacientes y Citas</span>
+        </p>
+
+        <Paciente />
+        <Paciente />
+        <Paciente />
+        <Paciente />        
+
       </div>
     </>
   )
---
+++ citas-project/src/components/Paciente.jsx
@@ -0,0 +1,34 @@
+
+
+export const Paciente = () => {
+  return (
+    <>
+        <div className="m-3 bg-white shadow-md px-5 py-10 rounded-xl">
+            <p className="font-bold mb-3 text-gary-700 uppercase">
+                Nombre: {""}
+                <span className="font-normal normal-case">Hook</span>
+            </p>
+
+            <p className="font-bold mb-3 text-gary-700 uppercase">
+                Propietario: {""}
+                <span className="font-normal normal-case">Juanito</span>
+            </p>
+
+            <p className="font-bold mb-3 text-gary-700 uppercase">
+                Email: {""}
+                <span className="font-normal normal-case">correo@correo.com</span>
+            </p>
+
+            <p className="font-bold mb-3 text-gary-700 uppercase">
+                Fecha Alta: {""}
+                <span className="font-normal normal-case">10 Diciembre de 2022</span>
+            </p>
+
+            <p className="font-bold mb-3 text-gary-700 uppercase">
+                Sintomas: {""}
+                <span className="font-normal normal-case">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sed tenetur fuga similique nemo, consequuntur aut, corporis ab harum error id voluptatum sit possimus debitis dicta dolore accusantium! Laudantium, atque odio.</span>
+            </p>
+            </div>
+    </>
+  )
+}
