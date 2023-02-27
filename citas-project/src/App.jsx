d813cac4493d9cf6562ca56eaf851bc5549bccc7
Will_09 <wbahoque71@gmail.com>
2023-02-27 15:09:27 -0500

    Se aprendio a pasar las props y se hicieron algunos ejemplos

--- citas-project/src/App.jsx
+++ citas-project/src/App.jsx
@@ -1,9 +1,21 @@
+import { useState } from 'react';
 import { Header } from './components/Header';
 import { Formulario } from './components/Formulario';
 import { ListadoPaciente } from './components/ListadoPaciente';
 
 
+
 function App() {
+
+  const [pacientes, setPacientes] = useState([]);
+
+  // const sumaPrueba = () => {
+  //   console.log(2 + 2);
+  // }
+
+  const toma1Valor = (valor) => {
+    console.log(valor);
+  }
   
   // <div className="App">
   //     <h1>{'Hello World'.toLowerCase()}</h1> {/* Se pone los corchetes para realizar metodos JS y estos no se muestren como textos, solo se imprimen expresiones*/}
@@ -12,7 +24,12 @@ function App() {
   return (
     <>
       <div className="container mx-auto mt-20">
-        <Header />
+        <Header
+          // numeros={1}
+          // isAdmin={false}
+          // fn={sumaPrueba}
+          toma1Valor={toma1Valor}
+        />
         <div className="mt-12 md:flex">
           <Formulario />
           <ListadoPaciente />
@@ -23,3 +40,7 @@ function App() {
 }
 
 export default App;
+
+
+// Los props o propiedades se utilizan para pasar los componentes del padre al hijo, para hacerlo viceversa no es reconmendable hacerlo y si se llegase a usar se tienen que hacer varias cosas. 
+// Si hay un useState que se va a pasar por diferentes componentes, lo mejor es colocarlo en el archivo principal
--- citas-project/src/components/Header.jsx
+++ citas-project/src/components/Header.jsx
@@ -1,6 +1,14 @@
 
 
-export const Header = () => {
+export const Header = ({toma1Valor}) => {
+
+    // console.log(numeros);
+    // console.log(isAdmin);
+    // fn();
+
+    const variableHeader = true;
+
+    toma1Valor(variableHeader);
 
     return(
 
@@ -17,3 +25,5 @@ export const Header = () => {
 }
 
 // Nota importante: Para llamar un componente en react lo que importa es que se mande la exportacion con la primera palabra en mayuscula, Los nombre que se le dan al componente al momento de crearlos se aplica lo mismo pero por "buenas practicas"
+
+// 2 Nota: Los componentes reciben las props por ese nombre, si se desea extraer el nombre de las props por como se declaran se hace un destructuring de objetos
