af88251aecb7ddeda8323ab8719b19b9cf7d53fa
Will_09 <wbahoque71@gmail.com>
2023-03-22 15:30:56 -0500

    Se configura las animaciones de la ventana modal

--- control-gastos/components/Modal.jsx
+++ control-gastos/components/Modal.jsx
@@ -1,9 +1,14 @@
 import CerrarModalImg from '../src/assets/img/cerrar.svg';
 
-export const Modal = ({setModal}) => {
+export const Modal = ({setModal, animarModal, setAnimarModal}) => {
 
     const ocultarModal = () => {
-        setModal(false); // Se recibe para volver a configurar la vista del setModal
+
+        setAnimarModal(false); // Lo mismo del anterior
+
+        setTimeout(() => {
+            setModal(false); // Se recibe para volver a configurar la vista del setModal            
+        }, 780);
     }
 
   return (
@@ -16,6 +21,10 @@ export const Modal = ({setModal}) => {
                     onClick={ocultarModal}
                 />
             </div>
+            <form className={`formulario ${animarModal ? "animar" : 'cerrar'}`}> 
+            {/* En la className anterior se puede ver como tratar clases css de manera dinamica en react */}
+                <legend>Nuevo Gasto</legend>
+            </form>
         </div>
     </>
   )
--- control-gastos/src/App.jsx
+++ control-gastos/src/App.jsx
@@ -10,9 +10,14 @@ export const App = () => {
   const [presupuesto, setPresupuesto] = useState(0); // Este useState es creado para controlar el presupuesto
   const [isValidPresupuesto, setIsValidPresupuesto] = useState(false); // Este useState sirve para para la validacion del presupuesto
   const [modal, setModal] = useState(false); // Se utiliza este State para controlar la visualizacion de la ventana modal
+  const [animarModal, setAnimarModal] = useState(false); // Este useState se utiliza para controlar la visualizacion de la animacion (css)
 
   const handleNuevoGasto = () => {
     setModal(true); // Aqui se habilita la vista del modal
+
+    setTimeout(() => {
+      setAnimarModal(true); // Aqui se cambia el useState para mostrar en el modal las animaciones
+    }, 870); // Este setTimeout se crea para tratar animaciones desde el css
   }
 
   return (
@@ -36,7 +41,9 @@ export const App = () => {
 
         {modal && 
         <Modal
-        setModal={setModal} // Se pasa como property para configurar la vista del modal
+          setModal={setModal} // Se pasa como property para configurar la vista del modal
+          animarModal={animarModal} // Lo mismo del anterior
+          setAnimarModal={setAnimarModal} // Lo mismo del anterior
         />}
       </div>
     </>  
