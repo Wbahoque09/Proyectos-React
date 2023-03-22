1269ad69d8301f3c44357b815fe831e6216affcb
Will_09 <wbahoque71@gmail.com>
2023-03-22 09:57:08 -0500

    Se formatea la data para mostrarse en vista

--- control-gastos/components/ControlPresupuesto.jsx
+++ control-gastos/components/ControlPresupuesto.jsx
@@ -1,6 +1,16 @@
 
 
 export const ControlPresupuesto = ({presupuesto}) => {
+
+    const formatearCantidad = (cantidad) => {
+        return Number(cantidad).toLocaleString('es-CO', {
+            style: 'currency',
+            currency: 'COP'
+        });
+    }
+
+    // console.log(formatearCantidad()); No sirve :V
+
   return (
     <>
         <div className="contenedor-presupuesto contenedor sombra dos-columnas">
@@ -9,7 +19,13 @@ export const ControlPresupuesto = ({presupuesto}) => {
             </div>
             <div className="contenido-presupuesto">
                 <p>
-                    <span>Presupuesto: </span> {presupuesto}
+                    <span>Presupuesto: </span> {formatearCantidad(presupuesto)}
+                </p>
+                <p>
+                    <span>Disponible: </span> {formatearCantidad(0)}
+                </p>
+                <p>
+                    <span>Gastado: </span> {formatearCantidad(0)}
                 </p>
             </div>
         </div>
