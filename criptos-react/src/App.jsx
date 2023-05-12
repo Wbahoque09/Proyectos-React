e7ad82c775f483c970566b21ff77230d0b4f5bd7
Will_09 <wbahoque71@gmail.com>
2023-05-12 14:23:18 -0500

    Se importa dependencias de styled components y se realiza la primera implementacion de estas

--- criptos-react/src/App.jsx
+++ criptos-react/src/App.jsx
@@ -1,10 +1,21 @@
 import { useState } from 'react';
+import styled from '@emotion/styled'; // Para utlizar styled component se empieza con esta importacion
+
+const Heading = styled.h1`
+  font-family: 'Nunito', sans-serif;
+  color: #FFF;
+`
 
 export const App = () => {
 
   return (
     <>
-      <h1>App location</h1>
+      <Heading>App location</Heading>
     </>
   )
 }
+
+
+
+
+// Nota con styled components, primero se añaden las respectivas dependencias para que funcionen "yarn add @emotion/react @emotion/styled", segundo se hace la importacion de la linea 2, tercero para utilizar y agregar estilos se hace el proceso que se ve en la linea 4 a la 7, otra nota aclaratorio los nombres de las variables "CSS" empiezan en mayusculas como los componentes de react y dentro del return para agregar estos estilos se cambia el elemnto html por nombre de la variable creada.
