0e1f997ab4c89fe5a30340abd45ce7f4e4f6b596
Will_09 <wbahoque71@gmail.com>
2023-05-12 15:53:47 -0500

    Se agrega mas contenido css al proyecto y se implementa la imagen

--- criptos-react/src/App.jsx
+++ criptos-react/src/App.jsx
@@ -1,16 +1,61 @@
 import { useState } from 'react';
 import styled from '@emotion/styled'; // Para utlizar styled component se empieza con esta importacion
+import imagenCripto from './assets/img/imagen-criptos.png';
 
+const Contenedor = styled.div`
+  max-width: 900px;
+  margin: 0 auto;
+  width: 90%;
+  @media (min-width: 992px) { // Tambien se puede implementar @mediaquerys
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    column-gap: 2rem;
+  }
+`
+
+const Imagen = styled.img`
+  max-width: 400px;
+  width: 80%;
+  margin: 100px auto 0 auto;
+  display: block;
+`
+
+// Este es el ejemplo de la nota de abajo.
 const Heading = styled.h1`
-  font-family: 'Nunito', sans-serif;
+  font-family: "Nunito", sans-serif;
   color: #FFF;
+  text-align: center;
+  font-weight: 700;
+  margin-top: 80px;
+  margin-bottom: 50px;
+  font-size: 34px;
+
+  &::after{
+    content: "";
+    width: 100px;
+    height: 6px;
+    background-color: #66A2FE;
+    display: block;
+    margin: 10px auto 0 auto;
+  }
+  /* el after es con se crea el la barra (investigar mas sobre esto) */
 `
 
 export const App = () => {
 
   return (
     <>
-      <Heading>App location</Heading>
+      <Contenedor>
+        <Imagen
+          src={imagenCripto}
+          alt="imagenes cripto"
+          // Para crear o insertar la imagen se declara como componente, debo investigar sobre el tema, se pasan como props los atributos de la imagen.  
+        />
+        <div>
+          <Heading>Cotiza Criptomonedas al instante</Heading>
+        </div>
+        
+      </Contenedor>
     </>
   )
 }
