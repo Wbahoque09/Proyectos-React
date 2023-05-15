d339ea49b0b9aa2ff98059196d7e771f5d705f9d
Will_09 <wbahoque71@gmail.com>
2023-05-15 09:55:15 -0500

    Se crea el input del formulario y se añaden sus respectivos diseños

--- criptos-react/src/components/Formulario.jsx
+++ criptos-react/src/components/Formulario.jsx
@@ -1,6 +1,22 @@
 import styled from '@emotion/styled';
 
+const InputSubmit = styled.input`
+    background-color: #9497FF;
+    border: none;
+    width: 100%;
+    padding: 10px;
+    color: #FFF;
+    font-weight: 700;
+    text-transform: uppercase;
+    font-size: 20px;
+    border-radius: 5px;
+    transition: background-color .3s ease; /* Investigar mas sobre este efecto */
 
+    &:hover{
+        background-color: #7A7DFE;
+        cursor: pointer;
+    }
+`
 
 export const Formulario = () => {
     return (
@@ -8,7 +24,7 @@ export const Formulario = () => {
             <form>
 
 
-                <input type="submit" value={"Cotizar"} />
+                <InputSubmit type="submit" value={"Cotizar"} />
             </form>
         </>
     )
