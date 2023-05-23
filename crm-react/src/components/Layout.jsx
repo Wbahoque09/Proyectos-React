6d8859269fb97ccabdf3c17c4fc41f6c1f4d6d3c
Will_09 <wbahoque71@gmail.com>
2023-05-23 11:38:42 -0500

    Se añade la logica para saber en que pagina estoy ubicado y resaltarlo en el aside

--- crm-react/src/components/Layout.jsx
+++ crm-react/src/components/Layout.jsx
@@ -1,11 +1,20 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, Link, useLocation } from 'react-router-dom';
 
 export const Layout = () => {
+
+    const location = useLocation(); // El useLocation sirve para poder ver la ubicacion y pasarla en el Link
+
     return (
         <>
             <div className="md:flex md:min-h-screen">
                 <aside className="md:w-1/4 bg-blue-900 px-5 py-10">
                     <h2 className="text-4xl font-black text-center text-white">CRM - Clientes</h2>
+
+                    <nav className="mt-10">
+                        {/* Ir a Generalidades para explicacion de <Link/> */}
+                        <Link className={`${location.pathname === "/" ? "text-blue-300" : "text-white"} text-2xl block mt-2 hover:text-blue-300`}to="/">Clientes</Link>
+                        <Link className={`${location.pathname === "/clientes/nuevo" ? "text-blue-300" : "text-white"} text-2xl block mt-2 hover:text-blue-300`} to="/clientes/nuevo">Nuevos Clientes</Link>
+                    </nav>
                 </aside>
 
                 <main className="md:w-3/4 p-10 md:h-screen overflow-scroll">
--- crm-react/src/docs/Generalidades-Rutas.md
+++ crm-react/src/docs/Generalidades-Rutas.md
@@ -2,4 +2,6 @@
 
 Outlet: El outlet en React Router es el espacio reservado en un componente donde se muestra el contenido de las rutas secundarias. Facilita el enrutamiento anidado y permite que el contenido de las rutas secundarias se renderice en el componente padre en el lugar designado por el outlet.  
 
+Link: Se utiliza para crear enlaces (links) navegables dentro de una aplicación. Proporciona una forma de navegar entre las diferentes rutas definidas en tu aplicación sin tener que recargar la página completa.
+
  -->
