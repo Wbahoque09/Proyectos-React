f336e3253aaeb7fbdff3e5e090f17ad8da28719e
Will_09 <wbahoque71@gmail.com>
2023-05-23 18:12:56 -0500

    No se por donde quede

--- crm-react/src/components/Cliente.jsx
+++ crm-react/src/components/Cliente.jsx
@@ -2,13 +2,19 @@
 
 export const Cliente = ({cliente}) => {
 
-    const {id,nombre,telefono,email,emppresa} = cliente; // Desestructuracion de cliente para acceder mas facil a sus atributos
+    const {id,nombre,telefono,email,empresa} = cliente; // Desestructuracion de cliente para acceder mas facil a sus atributos
 
     return (
         <>
-            <tr>
+            <tr className="border-b">
+                <td className="p-6 space-y-2">
+                    <p className="text-2xl text-gray-800">{nombre}</p>
+                    <p>{empresa}</p>
+                </td>
+
                 <td className="p-6">
-                    {nombre}
+                    <p className="text-gray-600"><span className="text-gray-800 uppercase font-bold">Email: </span>{email}</p>
+                    <p className="text-gray-600"><span className="text-gray-800 uppercase font-bold">Tel: </span>{telefono}</p>
                 </td>
             </tr>
         </>
