fd6486b8064cc52643743679b215ab1edb7bf8ff
Will_09 <wbahoque71@gmail.com>
2023-06-02 11:13:08 -0500

    Se termina de añadir estilos a la tabla

--- crm-react/src/components/Cliente.jsx
+++ crm-react/src/components/Cliente.jsx
@@ -16,6 +16,22 @@ export const Cliente = ({cliente}) => {
                     <p className="text-gray-600"><span className="text-gray-800 uppercase font-bold">Email: </span>{email}</p>
                     <p className="text-gray-600"><span className="text-gray-800 uppercase font-bold">Tel: </span>{telefono}</p>
                 </td>
+
+                <td className="p-6 flex gap-3">
+                    <button
+                        type="button"
+                        className="text-blue-600 hover:text-blue-700 uppercase font-bold text-xs"
+                    >
+                        Editar
+                    </button>
+
+                    <button
+                        type="button"
+                        className="text-red-600 hover:text-red-700 uppercase font-bold text-xs"
+                    >
+                        Eliminar
+                    </button>
+                </td>
             </tr>
         </>
     )
